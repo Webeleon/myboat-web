fe28568985da880bf979a575087c8ac62eb9cc1a
Julien Prugne <julien@webeleon.dev>
2021-05-29 12:14:40 +0200

    feat: add mandatory armament for offshore and semi_offshore navigation

--- data/mandatoryArmament.ts
+++ data/mandatoryArmament.ts
@@ -1,5 +1,7 @@
 import { navigationZone } from './navigationZone';
 
+const { BASIC, COASTAL, SEMI_OFFSHORE, OFFSHORE } = navigationZone;
+
 export interface MandatoryArmamentItem {
   name: string;
   description?: string;
@@ -10,122 +12,110 @@ export interface MandatoryArmamentItem {
 export const mandatoryArmament: MandatoryArmamentItem[] = [
   {
     name: 'Équipement individuel de flottabilité',
-    zones: [
-      navigationZone.BASIC,
-      navigationZone.COASTAL,
-      navigationZone.SEMI_OFFSHORE,
-      navigationZone.OFFSHORE,
-    ],
+    zones: [BASIC, COASTAL, SEMI_OFFSHORE, OFFSHORE],
   },
   {
     name: 'Dispositif lumineux',
-    zones: [
-      navigationZone.BASIC,
-      navigationZone.COASTAL,
-      navigationZone.SEMI_OFFSHORE,
-      navigationZone.OFFSHORE,
-    ],
+    zones: [BASIC, COASTAL, SEMI_OFFSHORE, OFFSHORE],
   },
   {
     name: "Moyens mobiles de lutte contre l'incendie",
-    zones: [
-      navigationZone.BASIC,
-      navigationZone.COASTAL,
-      navigationZone.SEMI_OFFSHORE,
-      navigationZone.OFFSHORE,
-    ],
+    zones: [BASIC, COASTAL, SEMI_OFFSHORE, OFFSHORE],
   },
   {
     name: "Dispositif d'assèchement manuel",
-    zones: [
-      navigationZone.BASIC,
-      navigationZone.COASTAL,
-      navigationZone.SEMI_OFFSHORE,
-      navigationZone.OFFSHORE,
-    ],
+    zones: [BASIC, COASTAL, SEMI_OFFSHORE, OFFSHORE],
   },
   {
     name: 'Dispositif de remorquage',
-    zones: [
-      navigationZone.BASIC,
-      navigationZone.COASTAL,
-      navigationZone.SEMI_OFFSHORE,
-      navigationZone.OFFSHORE,
-    ],
+    zones: [BASIC, COASTAL, SEMI_OFFSHORE, OFFSHORE],
   },
   {
     name: 'Ligne de mouillage (si masse lège ≥ 250 kgs)',
-    zones: [
-      navigationZone.BASIC,
-      navigationZone.COASTAL,
-      navigationZone.SEMI_OFFSHORE,
-      navigationZone.OFFSHORE,
-    ],
+    zones: [BASIC, COASTAL, SEMI_OFFSHORE, OFFSHORE],
   },
   {
     name: 'Annuaire des marées',
-    zones: [
-      navigationZone.BASIC,
-      navigationZone.COASTAL,
-      navigationZone.SEMI_OFFSHORE,
-      navigationZone.OFFSHORE,
-    ],
+    zones: [BASIC, COASTAL, SEMI_OFFSHORE, OFFSHORE],
   },
   {
     name: 'Pavillon national (hors eaux territoriales)',
-    zones: [
-      navigationZone.BASIC,
-      navigationZone.COASTAL,
-      navigationZone.SEMI_OFFSHORE,
-      navigationZone.OFFSHORE,
-    ],
+    zones: [BASIC, COASTAL, SEMI_OFFSHORE, OFFSHORE],
   },
   {
     name: "Dispositif de repérage et d'assistance pour personne à la mer",
-    zones: [
-      navigationZone.COASTAL,
-      navigationZone.OFFSHORE,
-      navigationZone.SEMI_OFFSHORE,
-    ],
+    zones: [COASTAL, OFFSHORE, SEMI_OFFSHORE],
   },
   {
     name: '3 feux rouges à main',
-    zones: [
-      navigationZone.COASTAL,
-      navigationZone.SEMI_OFFSHORE,
-      navigationZone.OFFSHORE,
-    ],
+    zones: [COASTAL, SEMI_OFFSHORE, OFFSHORE],
   },
   {
     name: 'Compas magnétique',
-    zones: [
-      navigationZone.COASTAL,
-      navigationZone.SEMI_OFFSHORE,
-      navigationZone.OFFSHORE,
-    ],
+    zones: [COASTAL, SEMI_OFFSHORE, OFFSHORE],
   },
   {
     name: 'Cartes marines officielles',
-    zones: [
-      navigationZone.COASTAL,
-      navigationZone.SEMI_OFFSHORE,
-      navigationZone.OFFSHORE,
-    ],
+    zones: [COASTAL, SEMI_OFFSHORE, OFFSHORE],
   },
   {
     name: 'Règlement international pour prévenir les abordages en mer',
-    zones: [
-      navigationZone.COASTAL,
-      navigationZone.SEMI_OFFSHORE,
-      navigationZone.OFFSHORE,
-    ],
+    zones: [COASTAL, SEMI_OFFSHORE, OFFSHORE],
   },
   {
     name: 'Description du système de balisage',
-    zones: [
-      navigationZone.COASTAL,
-      navigationZone.SEMI_OFFSHORE,
-      navigationZone.OFFSHORE,
-    ],
+    zones: [COASTAL, SEMI_OFFSHORE, OFFSHORE],
+  },
+  {
+    name: '3 fusées à parachute et 2 fumigènes ou 1 VHF fixe ',
+    zones: [SEMI_OFFSHORE, OFFSHORE],
+  },
+  {
+    name: 'Radeau de survie',
+    zones: [SEMI_OFFSHORE, OFFSHORE],
+  },
+  {
+    name: 'Matériel pour faire le point',
+    zones: [SEMI_OFFSHORE, OFFSHORE],
+  },
+  {
+    name: 'Livre des feux tenu à jour',
+    zones: [SEMI_OFFSHORE, OFFSHORE],
+  },
+  {
+    name: 'journal de bord',
+    zones: [SEMI_OFFSHORE, OFFSHORE],
+  },
+  {
+    name: 'Dispositif de réception des bulletins météorologiques',
+    zones: [SEMI_OFFSHORE, OFFSHORE],
+  },
+  {
+    name: 'Harnais et longe par navire pour les non voiliers',
+    zones: [SEMI_OFFSHORE, OFFSHORE],
+  },
+  {
+    name: 'Harnais et longe par personne embarquée pour les voiliers',
+    zones: [SEMI_OFFSHORE, OFFSHORE],
+  },
+  {
+    name: "Trousse de secours conforme à l'article 240-2,16",
+    zones: [SEMI_OFFSHORE, OFFSHORE],
+  },
+  {
+    name: 'Dispositif lumineux pour la recherche et le repérage de nuit',
+    zones: [SEMI_OFFSHORE, OFFSHORE],
+  },
+  {
+    name: 'Radiobalise de localisation des sinistres',
+    zones: [OFFSHORE],
+  },
+  {
+    name: 'VHF Fixe',
+    zones: [SEMI_OFFSHORE, OFFSHORE],
+  },
+  {
+    name: 'VHF Portative',
+    zones: [OFFSHORE],
   },
 ];
